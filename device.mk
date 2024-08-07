955c2d121ec13bb02d6667efaa0f548a37f2e0ca
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-07 18:57:45 +0300

    eqs: remove device-specific powershare
    Change-Id: Id499251c94ffbaa79c0a168dbf6dcb877e049dad
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- device.mk
+++ device.mk
@@ -98,10 +98,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_dne/android.hardware.nfc.ese.xml \
     frameworks/native/data/etc/android.hardware.se.omapi.ese.xml:$(TARGET_COPY_OUT_ODM)/etc/permissions/sku_dne/android.hardware.se.omapi.ese.xml
 
-# PowerShare
-PRODUCT_PACKAGES += \
-    vendor.lineage.powershare@1.0-service.eqs
-
 # SecureElement
 PRODUCT_PACKAGES += \
     android.hardware.secure_element@1.2-service-gto \
