298c012f8e652f7b2a804cfafa2321de77ad82cb
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-08 00:39:42 +0300

    eqs: remove device-specific sensors
    Change-Id: Icf2abe5dabf60742824922ab027d912a2b6a8753
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- device.mk
+++ device.mk
@@ -106,13 +106,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libse-gto-hal.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libse-gto-hal.conf
 
-# Sensors
-PRODUCT_PACKAGES += \
-    sensors.eqs
-
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += $(LOCAL_PATH)
 
