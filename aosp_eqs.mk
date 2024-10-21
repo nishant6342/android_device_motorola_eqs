2dff502e26f968c844ef98576f8a1e440a2ea6ad
ROMSG <romsg85@gmail.com>
2024-10-20 21:27:19 -0700

    eqs: Adjust override prop key names
    Change-Id: I7c2666496297e7db62d77b885002311c8658c441
    Signed-off-by: Nishant Kumar <www.rajsonu13@gmail.com>

--- aosp_eqs.mk
+++ aosp_eqs.mk
@@ -43,7 +43,6 @@ PRODUCT_MODEL := motorola edge 30 ultra
 PRODUCT_GMS_CLIENTID_BASE := android-motorola
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_PRODUCT=eqs_ge \
-    PRIVATE_BUILD_DESC="eqs_ge-user 14 U1SQS34.52-21-1-13 1b02e-87c13 release-keys"
-
-BUILD_FINGERPRINT := motorola/eqs_ge/eqs:14/U1SQS34.52-21-1-13/1b02e-87c13:user/release-keys
+    DeviceProduct="eqs_ge" \
+    BuildDesc="eqs_ge-user 14 U1SQS34.52-21-1-13 1b02e-87c13 release-keys" \
+    BuildFingerprint="motorola/eqs_ge/eqs:14/U1SQS34.52-21-1-13/1b02e-87c13:user/release-keys"
