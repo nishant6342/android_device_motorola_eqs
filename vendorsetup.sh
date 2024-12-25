a7c536ff9a963f3c35550ce79cf28deee1b51954
Nishant Kumar <www.rajsonu13@gmail.com>
2024-12-25 10:54:45 +0530

    eqs: Track VANY Branch

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,17 +18,17 @@ cd device/motorola/eqs && git reset --hard HEAD~1 && git pull;cd ../../..
 echo Done pulling changes, now proceeding to clone or pull changes of other repos
 echo eqs: start cloning device specific repos
 COMMON_DT=device/motorola/sm8475-common/sm8475.mk
-if ! [ -a $COMMON_DT ]; then git clone https://github.com/nishant6342/device_motorola_sm8475-common -b UNO device/motorola/sm8475-common
+if ! [ -a $COMMON_DT ]; then git clone https://github.com/nishant6342/device_motorola_sm8475-common -b VANY device/motorola/sm8475-common
 else
 cd device/motorola/sm8475-common && git reset --hard HEAD~1 && git pull;cd ../../..
 fi
 COMMON_VT=vendor/motorola/sm8475-common/sm8475-common-vendor.mk
-if ! [ -a $COMMON_VT ]; then git clone https://github.com/nishant6342/vendor_motorola_sm8475-common -b UNO vendor/motorola/sm8475-common
+if ! [ -a $COMMON_VT ]; then git clone https://github.com/nishant6342/vendor_motorola_sm8475-common -b VANY vendor/motorola/sm8475-common
 else
 cd vendor/motorola/sm8475-common && git reset --hard HEAD~1 && git pull;cd ../../..
 fi
 VT=vendor/motorola/eqs/eqs-vendor.mk
-if ! [ -a $VT ]; then git clone https://gitea.com/nishant6342/vendor_motorola_eqs -b UNO vendor/motorola/eqs
+if ! [ -a $VT ]; then git clone https://gitea.com/nishant6342/vendor_motorola_eqs -b VANY vendor/motorola/eqs
 else
 cd vendor/motorola/eqs && git reset --hard HEAD~1 && git pull;cd ../../..
 fi
