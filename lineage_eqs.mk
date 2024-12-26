8d2f7f41d6b68f083e95c40d2e9b334ea94f3389
Nishant Kumar <www.rajsonu13@gmail.com>
2024-12-26 16:14:37 +0530

    eqs: AOSP --> Lineage

--- lineage_eqs.mk
+++ lineage_eqs.mk
@@ -22,7 +22,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/motorola/eqs/device.mk)
 
 # Inherit some common Project Elixir stuff.
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Official-ify
 ELIXIR_BUILD_TYPE := OFFICIAL
