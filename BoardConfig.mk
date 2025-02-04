3be17e3d4b478fb2c3bbf76e76e49c3884b98894
Nishant Kumar <www.rajsonu13@gmail.com>
2025-02-04 17:53:57 +0530

    eqs: Drop custom vibrator effect lib

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -53,9 +53,5 @@ VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 BOARD_AVB_ROLLBACK_INDEX := 20
 BOARD_AVB_VBMETA_SYSTEM_ROLLBACK_INDEX := 20
 
-# Vibrator
-TARGET_QTI_VIBRATOR_EFFECT_LIB := libqtivibratoreffect.eqs
-TARGET_QTI_VIBRATOR_USE_EFFECT_STREAM := true
-
 # inherit from the proprietary version
 include vendor/motorola/eqs/BoardConfigVendor.mk
