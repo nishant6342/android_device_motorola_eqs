576b3b059eb2ff5c10c63962ebb56bf576e55b1d
Cosmin Tanislav <demonsingur@gmail.com>
2025-02-17 11:15:26 +0200

    eqs: do not manually build dependencies
    Change-Id: Ic2fc8da1da00636d10778f02ab07146c83d277cb

--- device.mk
+++ device.mk
@@ -79,10 +79,7 @@ $(foreach f,$(wildcard $(LOCAL_PATH)/rootdir/bin/*.sh),\
 
 # NFC
 PRODUCT_PACKAGES += \
-    android.hardware.nfc@1.2.vendor \
-    android.hardware.secure_element@1.2.vendor \
     com.android.nfc_extras \
-    libchrome.vendor \
     NfcNci \
     SecureElement \
     Tag
